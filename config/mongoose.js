d58a175864a2916d3d032716b5f0a706b157e91f
Rishi <rishikes.kr@gmail.com>
2020-07-06 18:59:15 +0530

    First commit

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+
+//connect db
+mongoose.connect('mongodb://localhost/Todolist_db');
+
+//ACQUIRE connection if successful
+const db=mongoose.connection;
+
+//if error
+db.on('error',console.error.bind(console,'error connecting to db'));
+
+//up and running
+db.once('open',function()
+{
+    console.log('successful connection to db');
+}) 
